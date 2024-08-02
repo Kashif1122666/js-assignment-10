93ef10e888c811406e4afc01eb02184a405a8668
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-02 16:09:41 -0700

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,65 @@
+// EXCERCISE SIX:
+// var arrayi = [];
+ 
+// for(var i = 0; i < 5; i++){
+//     var userinpt = +prompt("please enter a number");
+//     arrayi.push(userinpt);
+//   console.log(arrayi[i]);
+// }
+// let lowest = arrayi[0];
+// for(var i = 0; i<arrayi.length; i++){
+//     if(lowest > arrayi[i]){
+//         lowest = arrayi[i];
+//     }
+// }
+// console.log(`lowest number in given numbers  ${lowest}`);
+// ===============================================
+
+// EXCERCISE SEVEN:
+// converting celsius to fahrenheit
+
+// var celsius = +prompt("please enter temperature in celsius");
+// var fahrenheit = ((celsius * 9) / 5) + 32;
+// console.log(`Given temperature in fahrenheit ${fahrenheit} `);
+// ============================================= 
+
+// EXCERCISE EIGHT:
+// converting fahrenheit to celsius
+
+// var fahrenheit = +prompt("please enter temperature in fahrenheit");
+// var celsius = ((fahrenheit - 32) * 5) / 9;
+// console.log(`Given temperature in celsius ${celsius} `);
+// ====================================================
+
+// EXCERCISE NINE:
+// reverse an array
+
+// var arraye = [];
+// var opposite = [];
+// for(var i = 0; i < 5; i++){
+//     var userinpt = prompt("please enter an alphabet");
+//     arraye.push(userinpt);
+
+// }
+// for(var i = arraye.length-1; i >=0; i-- ){
+//     opposite.push(arraye[i]);
+//     console.log(arraye[i]);
+// }
+// console.log(`IN given order ${arraye}`);
+// console.log(`opposite of given order ${opposite}`);
+// =================================================
+
+// EXCERCISE TEN:
+// CALCULATE AVERAGE OF GIVEN ARRAY
+
+// var arrayy = [];
+// var sum = 0;
+// for(var i = 0; i < 5; i++){
+//     const userinput = +prompt("enter a number");
+//     arrayy.push(userinput);
+//   console.log(`${i}input.  ${arrayy[i]}`); 
+//       sum += userinput;
+// }
+//   console.log(`sum of all inputs ${sum}`);
+// console.log(`average of given array is =` + sum / arrayy.length);
+// ===============================================
